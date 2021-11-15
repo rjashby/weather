072279d1929c4e0d4003dc33276edf608abadd71
Frank-Proulx <frankproulx7@gmail.com>
2021-11-15 10:01:01 -0800

    add two more items to call

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+
+
+
+
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     const city = $('#location').val();
     $('#location').val("");
 
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`;
+    
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
@@ -21,10 +25,14 @@ $(document).ready(function() {
     request.open("GET", url, true);
     request.send();
 
-   function getElements(response) {
+    function getElements(response) {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      $('.showTemp').text(`The temperature in Fahrenheit is ${response.main.temp} degrees.`);
+      $('.feelsLike').text(`The real feel temperature is ${response.main.feels_like} degrees.`);
+      $('.windSpeed').text(`The wind speed is ${response.wind.speed} mph.`);
+      $('.rain3H').text(`The rainfall in the past 3 hours is ${response.rain["3h"]} inches.`);
     }
   });
 });
+
 
