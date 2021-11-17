10c80d69976d5419198c144d4940b6ef33cb48ec
Ryan Ashby <rjapsu160@gmail.com>
2021-11-17 08:00:36 -0800

    following along with lesson.

--- src/index.js
+++ src/index.js
@@ -2,37 +2,33 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './weather-service.js'
 
+function clearFields() {
+  $('#location').val("");
+  $('.showErrors').text("");
+  $('.showHumidity').text("");
+  $('.showTemp').text("");
+}
 
+function getElements(response) {
+  if (response.main) {
+    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+    $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response}`);
+  }
+}
 
-
+async function makeApiCall(city) {
+  const response = await WeatherService.getWeather(city);
+  getElements(response);
+}
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
-
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`;
-    
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    };
-
-    request.open("GET", url, true);
-    request.send();
-
-    function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Fahrenheit is ${response.main.temp} degrees.`);
-      $('.feelsLike').text(`The real feel temperature is ${response.main.feels_like} degrees.`);
-      $('.windSpeed').text(`The wind speed is ${response.wind.speed} mph.`);
-      $('.rain3H').text(`The rainfall in the past 3 hours is ${response.rain["3h"]} inches.`);
-    }
+    let city = $('#location').val();
+    clearFields();
+    makeApiCall(city);
   });
 });
-
-
